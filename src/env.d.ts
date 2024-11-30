b8ea48de517bbb8261565b85a9934b4adb77a355
Evenxlt <rmdc6487@gmail.com>
2024-11-30 14:25:00 -0300

    modificadothre

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module 'rehype';
+declare module 'hastscript';
+declare module 'hast-util-to-string';
+declare module 'hast-util-from-html';
+declare module 'hast-util-select';
+declare module 'vfile';
